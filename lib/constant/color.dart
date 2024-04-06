dd286374864b750ee94932534e09c977c7ae82e0
Bishal Shrestha <you@example.com>
2024-04-06 23:34:25 +0545

    changes

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/widgets.dart';
 class AppColor {
   static const Color primary = Color(0xFF0866FF);
   static const Color homeBGColor = Color(0xFFF0F2F5);
+  static const Color textColor = Color(0xFF828386);
 }
---
+++ lib/model/story_model.dart
@@ -0,0 +1,65 @@
+class StoryModel {
+  String name;
+  String storyImage;
+  String profileImage;
+
+  StoryModel(
+      {required this.name,
+      required this.storyImage,
+      required this.profileImage});
+}
+
+List<StoryModel> stories = [
+  StoryModel(
+      name: 'Ranjita Shrestha',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Nitesh Dhungana',
+      storyImage: 'assets/images/nitesh_story.jpg',
+      profileImage: 'assets/images/nitesh.jpg'),
+  StoryModel(
+      name: 'Prayash Rimal',
+      storyImage: 'assets/images/prayash_story.jpg',
+      profileImage: 'assets/images/prayash.jpg'),
+  StoryModel(
+      name: 'Sandesh Bidari',
+      storyImage: 'assets/images/sandesh_story.jpg',
+      profileImage: 'assets/images/sandesh.jpg'),
+  StoryModel(
+      name: 'Ram',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Hari',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Sita',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Gita',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'GBL',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Arjun',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Janak',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Anubhav',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+  StoryModel(
+      name: 'Hancy Kto Moh',
+      storyImage: 'assets/images/ranjita_story.jpg',
+      profileImage: 'assets/images/ranjita.jpg'),
+];
